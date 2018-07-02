774cd85d0923d190dfab1b0b0125849aa74cab3e
David Hou <david.hou314@gmail.com>
2018-07-02 11:12:34 -0700

    Scale for 48px display

--- generate_style.py
+++ generate_style.py
@@ -8,11 +8,13 @@ ROW_WIDTH = 255
 
 STYLE_TEMPLATE = '''.e{code} {{
 	background: no-repeat url(https://github.com/chaosagent/hangouts_emojis/raw/master/spritemap.png) -{pos_x}px, -{pos_y}px !important;
-	background-size: cover !important;
+	background-size: {size_x} {size_y} !important;
 }}'''
 
 image = Image.new('RGBA', (EMOJI_SIZE * ROW_WIDTH, EMOJI_SIZE * (len(emojis) // ROW_WIDTH + (1 if len(emojis) % ROW_WIDTH else 0))))
 styles = []
+
+DISPLAY_SIZE = 48
 
 for i, (filename, emoji) in enumerate(zip(filenames, emojis)):
 	this_im = Image.open(filename)
@@ -20,7 +22,13 @@ for i, (filename, emoji) in enumerate(zip(filenames, emojis)):
 	top = EMOJI_SIZE * (i // ROW_WIDTH)
 	box = (left, top, left + EMOJI_SIZE, top + EMOJI_SIZE)
 	image.paste(this_im.crop((0, 0, EMOJI_SIZE, EMOJI_SIZE)), box)
-	styles.append(STYLE_TEMPLATE.format(code=emoji, pos_x=left, pos_y=top))
+	styles.append(STYLE_TEMPLATE.format(
+		code=emoji,
+		pos_x=left * DISPLAY_SIZE / EMOJI_SIZE,
+		pos_y=top * DISPLAY_SIZE / EMOJI_SIZE,
+		size_x=image.size[0] * DISPLAY_SIZE / EMOJI_SIZE,
+		size_y=image.size[0] * DISPLAY_SIZE / EMOJI_SIZE,
+	))
 
 image.save('spritemap.png')
 
